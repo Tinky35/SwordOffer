b2db34d0dd95722c937d9c94b3b19b183369bf15
DINGBINGXUN <dingbingxun@163.com>
2025-01-07 20:13:36 +0800

    1、增加边界条件 2、更改调用形式，方便调用测试

--- 03_01_DuplicationInArray/C++/源.cpp
+++ 03_01_DuplicationInArray/C++/源.cpp
@@ -1,27 +1,58 @@
 #include <vector>
 #include <iostream>
 
+// 面试题3（一）：找出数组中重复的数字
+// 题目：在一个长度为n的数组里的所有数字都在0到n-1的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，
+// 也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。例如，如果输入长度为7的数组{2, 3, 1, 0, 2, 5, 3}，
+// 那么对应的输出是重复的数字2或者3。
 
-// 输出vec数据
-// 注意形参形式
-void coutVec(const std::vector<int>& data)
+
+// 比较结果
+void compareResault(const std::vector<int>& data, int expected[], int expectedSize)
 {
+    int size = data.size();
+    if (size == 0)
+    {
+        std::cout << "There are no duplicate numbers!" << std::endl;
+        return;
+    }
+
+    if (size != expectedSize)
+    {
+        std::cout << "The resault is wrong !" << std::endl;
+        return;
+    }
+
     for (int j = 0; j < data.size(); ++j)
     {
+        if (data.at(j) != expected[j])
+        {
+            std::cout << data.at(j) << " is not accort for：" << expected[j] << false;
+            return;
+        }
         std::cout << data[j] << "\t";
     }
+    std::cout << "The task is end!" << std::endl;
 }
 
-// 面试题3-数组中重复的数字（会修改数组）
-// 未设置边界保护
-void repeatNumFun()
+void repeatNumFun(int data[], int dataSize, int expected[], int expectedSize)
 {
-    int data[] = { 2, 3, 1, 0, 2, 5, 3 };
-    int dataSize = sizeof(data) / sizeof(data[0]);
+    if (dataSize <= 0 || data == nullptr)
+    {
+        std::cout << "The enter data is wrong!" << std::endl;
+        return;
+    }
+
     std::vector<int> repeatNum;
     for (int i = 0; i < dataSize; )
     {
         int value = data[i];
+        if (value < 0 || value > dataSize - 1)
+        {
+            std::cout << "Data is wrong!" << std::endl;
+            return;
+        }
+
         if (i == value)
         {
             ++i;
@@ -41,13 +72,30 @@ void repeatNumFun()
             }
         }
     }
-    coutVec(repeatNum);
+    compareResault(repeatNum, expected, expectedSize);
 }
 
 #ifndef RunTests
 int main() {
+    int numbers[] = { 2, 1, 3, 1, 4 };
+    int duplications[] = { 1 };
+    repeatNumFun(numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int));
 
-   
+    int numbers1[] = { 2, 4, 3, 1, 4 };
+    int duplications1[] = { 4 };
+    repeatNumFun(numbers1, sizeof(numbers1) / sizeof(int), duplications1, sizeof(duplications1) / sizeof(int));
+
+    int numbers2[] = { 2, 4, 2, 1, 4 };
+    int duplications2[] = { 2, 4 };
+    repeatNumFun(numbers2, sizeof(numbers2) / sizeof(int), duplications2, sizeof(duplications2) / sizeof(int));
+
+    int numbers3[] = { 2, 1, 3, 0, 4 };
+    int duplications3[] = { -1 }; // not in use in the test function
+    repeatNumFun(numbers3, sizeof(numbers3) / sizeof(int), duplications3, sizeof(duplications3) / sizeof(int));
+
+    int* numbers4 = nullptr;
+    int duplications4[] = { -1 }; // not in use in the test function
+    repeatNumFun(numbers4, sizeof(numbers4) / sizeof(int), duplications4, sizeof(duplications4) / sizeof(int));
 
     return 0;
 }
